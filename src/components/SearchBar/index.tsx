ab4a2a69c5f5ee4fe60ad6de2ac89477a3770c92
BruceWangyq <brucewangyq@gmail.com>
2022-03-08 16:20:44 -0500

    feat: clean unused files

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styles from "./index.module.css";
 import { useGraph } from "@/context/GraphContext";
 import { isValidAddr } from "@/utils/helper";
@@ -7,11 +6,10 @@ import { useWeb3 } from "@/context/web3Context";
 export const SearchBar: React.FC = () => {
     const { setGraphAddress, setSelectAddress } = useGraph();
     const { getAddressByEns } = useWeb3();
-    const [value, setValue] = useState("");
 
     const handleInputChange = async (e: { target: { value: string } }) => {
         const newValue = e.target.value;
-        setValue(newValue);
+
         if (newValue.slice(-3) === "eth") {
             try {
                 const ensAddr = await getAddressByEns(newValue);
