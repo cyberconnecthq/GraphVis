915fcc8383a79747d400d5cc240f327a4a00ae2f
BruceWangyq <brucewangyq@gmail.com>
2022-03-07 20:42:14 -0500

    feat: fixed walletconnect bug

--- src/components/WalletConnectButton/index.tsx
+++ src/components/WalletConnectButton/index.tsx
@@ -3,11 +3,10 @@ import LoadingButton from "@mui/lab/LoadingButton";
 import { useWeb3 } from "@/context/web3Context";
 import { formatAddress } from "@/utils/helper";
 import styles from "./index.module.css";
-import { useGraph } from "@/context/GraphContext";
 
 export const WalletConnectButton: React.FC = () => {
     const { connectWallet, address, ens } = useWeb3();
-    const { identity, setGraphAddress } = useGraph();
+
     const [loading, setLoading] = useState<boolean>(false);
 
     const connect = useCallback(async () => {
@@ -32,41 +31,9 @@ export const WalletConnectButton: React.FC = () => {
                     Connect Wallet
                 </LoadingButton>
             ) : (
-                <div className={styles.userInfo}>
-                    <div className={styles.avatarSection}>
-                        {identity.avatar ? (
-                            <a
-                                rel="noreferrer"
-                                href={
-                                    "https://app.cyberconnect.me/address/" +
-                                    identity?.address
-                                }
-                                target={"_blank"}
-                            >
-                                <img
-                                    src={identity.avatar}
-                                    alt={""}
-                                    width={40}
-                                    height={40}
-                                    className={styles.avatar}
-                                    onClick={() => setGraphAddress(address)}
-                                />
-                            </a>
-                        ) : (
-                            <a onClick={() => setGraphAddress(address)}>
-                                <img
-                                    src={"/Sample_User_Icon.png"}
-                                    alt={""}
-                                    width={40}
-                                    height={40}
-                                    className={styles.avatar}
-                                />
-                            </a>
-                        )}
-                    </div>
-                    <div className={styles.userAddress}>
-                        {ens || formatAddress(address)}
-                    </div>
+                <div className={styles.userAddress}>
+                    Address:
+                    {ens || formatAddress(address)}
                 </div>
             )}
         </>
